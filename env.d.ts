1cfcf2a5dd577e725b24040c0105935d1d8e9918
INSSTU <147832788+INSSTU@users.noreply.github.com>
2024-06-11 11:56:46 +0800

    环境变量的配置

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+//  可以编写一些项目环境变量的类型描述
+interface ImportMetaEnv {
+  VITE_HELLO: string
+  VITE_API: string
+}
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+console.log(import.meta.env.VITE_HELLO);
+
 </script>
 
 <template>
