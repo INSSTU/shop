b3112008d52a06130e31fe0b7f8b674f1484b750
INSSTU <147832788+INSSTU@users.noreply.github.com>
2024-06-12 10:42:29 +0800

    写ProductList样式

--- src/views/products/ProductList.vue
+++ src/views/products/ProductList.vue
@@ -1,22 +1,47 @@
 <script setup lang="ts">
-
 // 创建一个ref数组
 const products = ref<Product[]>([])
 
 // 加载数据
-fetch(import.meta.env.VITE_BASE_API + "/products")
-.then((res) => res.json() as Promise<Page<Product>>)
-.then((data) => (products.value = data.data))
+fetch(import.meta.env.VITE_BASE_API + '/products')
+  .then((res) => res.json() as Promise<Page<Product>>)
+  .then((data) => (products.value = data.data))
 </script>
 
 <template>
-  <div class="product" v-for="item in products" :key="item.id">
-    <div class="img-wrap">
-      <img :src="item.image_url" :alt="item.name">
+  <div class="product-list">
+    <div class="product" v-for="item in products" :key="item.id">
+      <div class="img-wrap">
+        <img :src="item.image_url" :alt="item.name" />
+      </div>
+      <h2>{{ item.name }}</h2>
+      <h3>{{ item.price }}</h3>
     </div>
-    <h2>{{ item.name }}</h2>
-    <h3>{{ item.price }}</h3>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.product-list {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  .product {
+    padding: 20rem;
+    .img-wrap {
+      border-radius: 20px;
+      overflow: hidden;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
+
+    .name {
+      font-size: 15rem;
+      color: var(--light-color);
+    }
+
+    .price {
+      &::before {
+        content: '￥';
+      }
+    }
+  }
+}
+</style>
