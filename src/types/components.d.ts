4998a203516f59693073a31c8fa330cf4a90ffb8
INSSTU <147832788+INSSTU@users.noreply.github.com>
2024-06-12 09:53:01 +0800

    home页面搭建（CategoryList）

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -7,10 +7,16 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    App: typeof import('./../App.vue')['default']
     BottomNav: typeof import('./../layout/BottomNav.vue')['default']
+    Cart: typeof import('./../views/Cart.vue')['default']
+    CategoryList: typeof import('./../views/home/CategoryList.vue')['default']
     Hello: typeof import('./../components/Hello.vue')['default']
+    Home: typeof import('./../views/home/Home.vue')['default']
     Layout: typeof import('./../layout/Layout.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Search: typeof import('./../views/Search.vue')['default']
+    User: typeof import('./../views/User.vue')['default']
   }
 }
---
+++ src/views/home/Home.vue
@@ -0,0 +1,16 @@
+import SvgIcon from '../types/svg-component';
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="title">成店</h1>
+
+    <CategoryList></CategoryList>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  text-align: center;
+}
+</style>
